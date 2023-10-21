ca1c15f329994a027528fbe89ad13246373b5b3f
Supakrit Nithikethkul <6410615139@student>
2023-10-22 00:50:10 +0700

    working not logic proof

--- pub_mqtt.py
+++ pub_mqtt.py
@@ -4,12 +4,15 @@ import RPi.GPIO as GPIO
 from gpiozero import Button
 import paho.mqtt.client as mqtt
 import argparse
+import time
 
+def signal_handler(sig, frame):
+        GPIO.cleanup()
+        sys.exit(0)
 def on_publish(client, userdata, mid):
     print("message published")
 def on_pre_connect(client, userdata):
     print("About to connect to the broker...")
-
 parser = argparse.ArgumentParser(description="MQTT Publisher")
 parser.add_argument("--broker", required=True, help="MQTT broker address")
 parser.add_argument("--port", type=int, default=1883, help="MQTT broker port")
@@ -20,15 +23,12 @@ parser.add_argument("--client_id", default="mqtt_publisher", help="Client ID")
 args = parser.parse_args()
 
 client = mqtt.Client(args.client_id)
+client.on_pre_connect = on_pre_connect
 client.on_publish = on_publish
 client.connect(args.broker, args.port, keepalive=60)
 
 # start a new thread
 client.loop_start()
-    
-def signal_handler(sig, frame):
-	GPIO.cleanup()
-	sys.exit(0)
 
 class Joystick:
 	def __init__(self, pin_left, pin_right, pin_top, pin_down, pin_center):
@@ -48,25 +48,25 @@ class Joystick:
 			if self.center_value > 0:
 				self.center_value -= len
 		if Button(self.pin_left).is_pressed:
-			if self.x_value > 0:
+			if self.x_value > -180:
 				self.x_value -= len
 		if Button(self.pin_right).is_pressed:
-			if self.x_value < 255:
+			if self.x_value < 180:
 				self.x_value += len
 		if Button(self.pin_top).is_pressed:
-			if self.y_value < 255:
+			if self.y_value < 100:
 				self.y_value += len
 		if Button(self.pin_down).is_pressed:
-			if self.y_value > 0:
+			if self.y_value > -100:
 				self.y_value -= len
 	def get_x(self):
 		return self.x_value
-
 	def get_y(self):
 		return self.y_value
-
 	def get_center(self):
 		return self.center_value
+	def get_center_bit(self):
+		return Button(self.pin_center).is_pressed
 
 button_left = 17
 button_right = 18
@@ -77,15 +77,16 @@ button_center = 22
 joystick = Joystick(button_left, button_right, button_top, button_down, button_center)
 signal.signal(signal.SIGINT, signal_handler)
 while True:
- try:
-  joystick.update(8)
-  x = joystick.get_x()
-  y = joystick.get_y()
-  center = joystick.get_center()
-  msg = f"{x} {y} {center}"
-  print(msg)
-  pubMsg = client.publish(args.topic, args.message, payload=msg.encode('utf-8'), qos=args.qos)
-  pubMsg.wait_for_publish()
-  print(pubMsg.is_published())
- except Exception as e:
-  print(e)
+	try:
+		joystick.update(8)
+		x = joystick.get_x()
+		y = joystick.get_y()
+		center = joystick.get_center_bit()
+		msg = "{0:4.0f} {1:4.0f} {2:1.0f}".format(x, y, center)
+		print(msg)
+		pubMsg = client.publish(args.topic, args.message, qos=args.qos)
+		pubMsg.wait_for_publish()
+		print(pubMsg.is_published())
+	except Exception as e:
+		print(e)
+	time.sleep(2)
